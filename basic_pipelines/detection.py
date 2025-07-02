30e02fed9ebfc0070e06b193a2a39075f71d2f96
abhinnvyas <abhinnvyas2@gmail.com>
2025-07-02 12:01:00 +0530

    mavlink_script in temp directory in basic_pipelines

--- basic_pipelines/detection.py
+++ basic_pipelines/detection.py
@@ -71,16 +71,16 @@ def app_callback(pad, info, user_data):
             detection_count += 1
         else:
             roi.remove_object(detection)
-    if user_data.use_frame:
-        # Note: using imshow will not work here, as the callback function is not running in the main thread
-        # Let's print the detection count to the frame
-        cv2.putText(frame, f"Detections: {detection_count}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        # Example of how to use the new_variable and new_function from the user_data
-        # Let's print the new_variable and the result of the new_function to the frame
-        cv2.putText(frame, f"{user_data.new_function()} {user_data.new_variable}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        # Convert the frame to BGR
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        user_data.set_frame(frame)
+    # if user_data.use_frame:
+    #     # Note: using imshow will not work here, as the callback function is not running in the main thread
+    #     # Let's print the detection count to the frame
+    #     cv2.putText(frame, f"Detections: {detection_count}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    #     # Example of how to use the new_variable and new_function from the user_data
+    #     # Let's print the new_variable and the result of the new_function to the frame
+    #     cv2.putText(frame, f"{user_data.new_function()} {user_data.new_variable}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    #     # Convert the frame to BGR
+    #     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    #     user_data.set_frame(frame)
  
     print(string_to_print)
     return Gst.PadProbeReturn.OK
